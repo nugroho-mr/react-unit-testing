6a4be883cc4e40d395a3640a8c21d892ac0d1dab
Panca Satrio Nugroho <nugroho.panca@gmail.com>
2022-06-02 19:45:16 +0700

    new  updates

--- src/Components/TodoFooter/TodoFooter.js
+++ src/Components/TodoFooter/TodoFooter.js
@@ -6,7 +6,7 @@ const TodoFooter = props => {
   return (
     <div className={style.todoFooter}>
         { !props.todoLength &&
-            <p data-testid="todo-footer-no-item">Awesome!! You have free time!!</p>
+            <p data-testid="todo-footer-no-item">Awesome!! You have free time now!!</p>
         }
         {props.todoLength > 0 &&
             <p data-testid="todo-footer-with-items">You have {props.todoLength} {props.todoLength > 1 ? 'tasks' : 'task' }</p>
